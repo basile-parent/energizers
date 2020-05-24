0859edf270d3fe1f7ce6ad43abf30b5cced8c913
Casou <casou.acc@gmail.com>
2020-05-24 23:42:40 +0200

    Fix deploy

--- front/walk-stop/js/constants.js
+++ front/walk-stop/js/constants.js
@@ -1,3 +1,2 @@
-// const WEBSOCKET_URL = 'http://localhost:7500';
-const WEBSOCKET_PATH = '/ws/energizers';
-const WEBSOCKET_URL = 'http://192.168.0.24' + WEBSOCKET_PATH;
+const WEBSOCKET_URL = 'http://localhost:3000';
+const WEBSOCKET_PATH = '';
